401fde109796b4cdac68c1e03c0c123323a37f2c
Tom <yj889200@gmail.com>
2014-03-04 10:35:50 +1100

    frequency analysis

--- diagrams/matlab_resource/drawFigure.m
+++ diagrams/matlab_resource/drawFigure.m
@@ -2,7 +2,7 @@ close all;
 clear all;
 clc;
 MaxValue = 64;
-dir = '..\frequency_count.csv';
+dir = '../frequency_count.csv';
 M=csvread(dir);
 
 
@@ -10,7 +10,10 @@ M=csvread(dir);
 
 %find all pattern value in column 1 as x, store in C
 A = [0,17,34,51,68, 85, 102, 119, 136, 153, 170, 187,204, 221, 238, 255];
-B = A;
+X = 0:255;
+
+%B = A;
+B = setdiff(X,A);
 C = [];
 for i=A
     for j=B
@@ -24,11 +27,11 @@ M1 = M(C,2)/MaxValue;
 S = 21760:21780;
 set (gcf,'Position',[400,100,1000,400]);
 %plot(C,M1,'*');
-%scatter(C,M1,5,'fill','r');
+scatter(C,M1,5,'fill','r');
 
 %plot(M(:,1),M(:,2)/MaxValue);
 %scatter(M(:,1),M(:,2)/MaxValue,5,'fill');
-bar(S,M(S,2)/MaxValue);
+%bar(S,M(S,2)/MaxValue);
 ylim([0 1]);
 
 xlabel('Decimal Value of Input Message Block');
