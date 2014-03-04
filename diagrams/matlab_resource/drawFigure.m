1b814c558b96393a99a12292542328053469ed5f
Tom Lau <active52004@gmail.com>
2014-03-04 11:10:45 -0800

    freqeuncy total diagram

--- diagrams/matlab_resource/drawFigure.m
+++ diagrams/matlab_resource/drawFigure.m
@@ -2,7 +2,7 @@ close all;
 clear all;
 clc;
 MaxValue = 64;
-dir = '../frequency_count.csv';
+dir = '..\frequency_count.csv';
 M=csvread(dir);
 
 
@@ -11,23 +11,52 @@ M=csvread(dir);
 %find all pattern value in column 1 as x, store in C
 A = [0,17,34,51,68, 85, 102, 119, 136, 153, 170, 187,204, 221, 238, 255];
 X = 0:255;
-
 %B = A;
 B = setdiff(X,A);
+D = B;
 C = [];
+for i=D
+    for j=B
+       C = [C i*256+j+1]; 
+    end
+end
+M1 = M(C,2)/MaxValue;
+scatter(C,M1,5,'fill','b');
+
+hold on
+C=[];
+B = A;
+for i=A
+    for j=B
+       C = [C i*256+j+1]; 
+    end
+end
+M1 = M(C,2)/MaxValue;
+scatter(C,M1,5,'fill','r');
+
+hold on
+C=[];
+B = setdiff(X,A);
 for i=A
     for j=B
        C = [C i*256+j+1]; 
     end
 end
 
-%use C as x M(C,2) as y to draw the bar
-
+for i=B
+    for j=A
+       C = [C i*256+j+1]; 
+    end
+end
 M1 = M(C,2)/MaxValue;
+scatter(C,M1,5,'fill','g');
+%use C as x M(C,2) as y to draw the bar
+legend('no-pattern and no-pattern','pattern and pattern', 'pattern and no-pattern')
+
 S = 21760:21780;
 set (gcf,'Position',[400,100,1000,400]);
 %plot(C,M1,'*');
-scatter(C,M1,5,'fill','r');
+scatter(C,M1,5,'fill','g');
 
 %plot(M(:,1),M(:,2)/MaxValue);
 %scatter(M(:,1),M(:,2)/MaxValue,5,'fill');
